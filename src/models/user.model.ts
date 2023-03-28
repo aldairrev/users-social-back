9e4a1986f25a0d4e3ce6191cdc1fef2a01cbd841
Aldair Revilla <53498934+aldairrev@users.noreply.github.com>
2023-03-27 22:47:56 -0500

    Update user.model.ts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,6 +3,9 @@ import { Schema, model } from 'npm:mongoose@7';
 const User = new Schema({
     firstname: String,
     lastname: String,
+    age: String,
+    bio: String,
+    subjects: String,
     avatar: String,
     enabled: Boolean,
 }, {
